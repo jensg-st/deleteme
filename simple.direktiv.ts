66c1947be7c4f268c3549e879acf810fcdf87b18
Jens Gerke <jens.gerke@direktiv.io>
2024-06-05 14:49:01 +0200

    hello
    Signed-off-by: Jens Gerke <jens.gerke@direktiv.io>

--- simple.direktiv.ts
+++ simple.direktiv.ts
@@ -6,17 +6,20 @@ const fn = setupFunction({
 
 function start(state) {
  
-
-
     var s = getSecret({
         name: "jens"
-    })
- 
+    }) 
  
     var file = getFile({
         name: "content.txt"
         scope: "shared"
     })
+
+    fn.execute(
+        {
+            input: "hello"
+        }
+    )
  
     return file.base64()
 }
